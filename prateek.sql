2689dd487ed4fa585460d8f8e408eef3e658ca4f
PrateekVenkat <137770680+PrateekVenkat@users.noreply.github.com>
2023-07-08 19:48:55 +0530

    Update prateek.sql

--- prateek.sql
+++ prateek.sql
@@ -4,5 +4,5 @@ create table Login(username varchar(50),password varchar(50));
 select*from Login;
 create table Product(productId int,productName varchar(20),minSell_Quantity int,price int,quantity int);
 select*from Product;
-insert into Product values(1,'abc',12,100,50);
+
 insert into Login values( 'aaa','123');
